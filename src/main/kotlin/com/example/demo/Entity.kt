385d4f7a5c78a4776db9d080f20b5cd621b3d5b8
wonjun3991 <wonjun.choi@meshkorea.net>
2023-01-07 19:22:15 +0900

    add Entity Article, User

---
+++ src/main/kotlin/com/example/demo/Entity.kt
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
+
+@Entity
+class Article(
+    var title: String,
+    val headline: String,
+    var content: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null
+)
+
+class User(
+    var login: String,
+    var firstName: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null
+)
